a4bad33261822da831c1017a6201c31a897765e5
Sevastopol12 <chkalov253@gmail.com>
2025-05-21 15:05:47 +0700

    feat: define controller for order commiment

---
+++ controllers/OrderController.js
@@ -0,0 +1,21 @@
+import OrderManager from "../models/Manager/order_manager.js";
+
+const om = new OrderManager()
+
+export async function handleCommitOrder(req, res, next) {
+    try {
+        let { orderMeta } = req.body;
+        let { products, address, payment } = orderMeta;
+        // Create order
+        om.create(products);
+        om.assignAddress(address);
+        om.addPayment(payment);
+
+        // Commit to DB
+        const mess = await om.add();
+        return res.status(200).json({ mess });
+    } 
+    catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+}
