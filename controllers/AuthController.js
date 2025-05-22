38a1c4cc6573102ec4c204411cc3fc600ed7593c
Sevastopol12 <chkalov253@gmail.com>
2025-05-22 14:02:47 +0700

    Remove homepage-redirect when logout

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -32,7 +32,6 @@ export function logout(req, res) {
   req.session.destroy(err => {
     if (err) return res.status(500).json({ message: 'Logout failed.' });
     res.clearCookie('connect.sid');
-    res.redirect('/');
   });
 }
 
